ddd95e1fca115e677862dbeaaca30a8041004cb5
Faith Magras <bdanielle092@gmail.com>
2020-11-20 11:59:19 -0600

    working on phase 2

--- planYourHeistAgain/Program.cs
+++ planYourHeistAgain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace planYourHeistAgain
 {
@@ -6,14 +7,43 @@ namespace planYourHeistAgain
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Plan Your Heist!");
-            Console.WriteLine("Enter team members name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter skill level");
-            int skillLevel = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter courage factor");
-            decimal courageFactor = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Team member info: Name: {name}, Skill Level: {skillLevel}, Courage Factor: {courageFactor}");
+            //declared what name is
+            string name = null;
+            //creating an empty list
+            List<TeamMember> teammates = new List<TeamMember>();
+            //while loop to run through the team members
+            while (name != "")
+            {
+                Console.WriteLine("Enter team members name");
+                name = Console.ReadLine();
+                // stops if they don't enter a name
+                if (name == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Enter skill level");
+                int skillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter courage factor");
+                decimal courageFactor = int.Parse(Console.ReadLine());
+                //creating the team members
+                TeamMember newTeammate = new TeamMember()
+                {
+                    Name = name,
+                    SkillLevel = skillLevel,
+                    CourageFactor = courageFactor
+                };
+                //addding the team member
+                teammates.Add(newTeammate);
+            }
+            //display the team members
+            foreach (TeamMember teammate in teammates)
+            {
+                Console.WriteLine(teammate);
+
+            }
+
 
         }
     }
--- planYourHeistAgain/TeamMember.cs
+++ planYourHeistAgain/TeamMember.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace planYourHeistAgain
 {
@@ -8,10 +9,5 @@ namespace planYourHeistAgain
         public int SkillLevel { get; set; }
         public decimal CourageFactor { get; set; }
 
-
-        public void AddTeamMember()
-        {
-
-        }
     }
 }
